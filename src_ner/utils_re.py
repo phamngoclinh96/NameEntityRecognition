691268a73bd957b65468e498b66aa19344e5b7fa
linhpn8 <linhpn8@viettel.com.vn>
2018-03-21 17:36:14 +0700

    Refactor NER

---
+++ src_ner/utils_re.py
@@ -0,0 +1,20 @@
+import re
+
+
+
+# expressions =[
+#     r"[A-Z]",
+#     r"[0-9]{10,12}",
+#     r"[0-9]+",
+#     r"(?:[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F])+",
+#     r"([a-zA-Z0-9][a-zA-Z0-9_.+-]*@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)+)",
+#     r"(?:ftp|http|https)?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+",
+#     r"[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)+",
+#     r"(?:[0-9]+\.)+[0-9]+",
+#     r"(?:[0-9]+\.){3}[0-9]+",
+#     r"(?:[0-9]+\.){5}[0-9]+",
+#     r"(?:AS|as)[0-9]+",
+#     r"(?:CVE|exploit)",
+#     r"(?:HK|hk)[a-zA-Z0-9/]+"]
+def get_pattern(token,expressions):
+    return [len(re.findall(expression,token)) for expression in expressions]
